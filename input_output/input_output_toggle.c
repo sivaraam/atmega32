e22e9c473a53a4ab82987cc518b5170d50935e61
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-03-28 16:54:11 +0530

    input_output: code cleanup  the toggle test implementation
    - Try to use macros for port names
    
    - avoid mixed use of C-style (/* ... */) and C++-style comments (// ...)
    
    - adding more comments

--- input_output/input_output_toggle.c
+++ input_output/input_output_toggle.c
@@ -2,11 +2,16 @@
  * Simple program to see what happens if we toggle the data
  * direction of the pins at some time intervals and whether
  * it behaves as intended.
+ *
+ * PORTB - configured as output for debug
+ * PORTA - configured as input and then as output
  */
 
 #include <avr/io.h>
 #define F_CPU 1000000UL
 #include <util/delay.h>
+
+#define DBG_PORT PORTB
 
 int main (void)
 {
@@ -14,36 +19,44 @@ int main (void)
 
 	while (1)
 	{
-		PORTB = 0xFF;             // LED Off
-		_delay_ms (500);
-		PORTB ^= 0xFF;            // LED On
-		_delay_ms (500);
-		PORTB ^= 0xFF;            // LED Off
-		_delay_ms (500);
 
-		// wait for some time
-		_delay_ms (1000);
+		/*
+		 * Test port as input. Input is directly assigned to output.
+		 */
+		DBG_PORT = 0xFF;             /* LED Off */
+		_delay_ms (500);
+		DBG_PORT ^= 0xFF;            /* LED On */
+		_delay_ms (500);
+		DBG_PORT ^= 0xFF;            /* LED Off */
+		_delay_ms (500);
 
 		DDRA = 0x00;
-		PORTB = PINA;
 
-		// wait for some time
+		/* wait for some time */
+		_delay_ms (1000);
+
+		DBG_PORT = PINA;
+
+		/* wait for some time */
 		_delay_ms (1500);
 
-		PORTB = 0xFF;             // LED Off
+		/*
+		 * Test port as output.
+		 */
+		DBG_PORT = 0xFF;             /* LED Off */
 		_delay_ms (500);
-		PORTB ^= 0xFF;            // LED On
+		DBG_PORT ^= 0xFF;            /* LED On */
 		_delay_ms (500);
-		PORTB ^= 0xFF;            // LED Off
+		DBG_PORT ^= 0xFF;            /* LED Off */
 		_delay_ms (500);
-		PORTB ^= 0xFF;            // LED On
+		DBG_PORT ^= 0xFF;            /* LED On */
 		_delay_ms (500);
-		PORTB ^= 0xFF;            // LED Off
+		DBG_PORT ^= 0xFF;            /* LED Off */
 		_delay_ms (500);
 
 		DDRA = 0xFF;
 		PORTA = 0xFF;
-		// toggle the LEDs for a few times
+		/* toggle the LEDs for a few times */
 		unsigned count = 10;
 		while (count > 0)
 		{
