8a4f0010971984c9cbcf3c947561e45e0888e2b2
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-03-28 21:35:07 +0530

    i2c_rtc: change the default values for the data and time
    This is to observe if all values change.

--- i2c_rtc/rtc.c
+++ i2c_rtc/rtc.c
@@ -189,10 +189,10 @@ RTC_init (void)
 		 * Seconds register (Address: 0x00):
 		 *
 		 * Clock Hold (CH) (7): 0 (Starts the clock)
-		 * 10s digit of seconds (6-4): 0
+		 * 10s digit of seconds (6-4): 5
 		 * 1s digit of seconds (3-0): 0
 		 */
-		I2C_send (0x00) ||
+		I2C_send (0x50) ||
 
 		/*
 		 * Minutes register (Address: 0x01):
@@ -208,36 +208,36 @@ RTC_init (void)
 		 *
 		 * (7): 0
 		 * 12/24-hour clock (6): 0 (24-hour clock)
-		 * 10s digit of hours (5-4): 1
-		 * 1s digit of hours (3-0): 2
+		 * 10s digit of hours (5-4): 2
+		 * 1s digit of hours (3-0): 3
 		 */
-		I2C_send (0x12) ||
+		I2C_send (0x23) ||
 
 		/*
 		 * Day register (Address: 0x03)
 		 *
 		 * (7-3): 0
-		 * Day (2-0): 03 (Wednesday; Week starts from Monday; 1-indexed)
+		 * Day (2-0): 01 (Wednesday; Week starts from Monday; 1-indexed)
 		 */
-		I2C_send (0x03) ||
+		I2C_send (0x01) ||
 
 		/*
 		 * Date register (Address: 0x04)
 		 *
 		 * (7-6): 0
-		 * 10s digit of date (5-4): 2
-		 * 1s digit of date (3-0): 8
+		 * 10s digit of date (5-4): 3
+		 * 1s digit of date (3-0): 1
 		 */
-		I2C_send (0x28) ||
+		I2C_send (0x31) ||
 
 		/*
 		 * Month register (Address: 0x05)
 		 *
 		 * (7-5): 0
-		 * 10s digit of month (4): 0
-		 * 1s digit of month (3-0): 3
+		 * 10s digit of month (4): 1
+		 * 1s digit of month (3-0): 2
 		 */
-		I2C_send (0x03) ||
+		I2C_send (0x12) ||
 
 		/*
 		 * Year register (Address: 0x06)
