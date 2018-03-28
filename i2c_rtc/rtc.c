f214c79b4f5615b886d7e1e166ea48b9b8fce008
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-03-28 19:55:47 +0530

    i2c_rtc: initialize "all" registers in the RTC with custom defaults

--- i2c_rtc/rtc.c
+++ i2c_rtc/rtc.c
@@ -29,33 +29,6 @@ static const uint8_t seconds_addr = 0x00;
 int8_t
 RTC_init (void)
 {
-	static const uint8_t seconds_addr = 0x00,
-	                     control_addr = 0x07,
-	                     seconds_config_word = 0x00,
-	                     minutes_config_word = 0x59,
-	                     hours_config_word = 0x12,
-	                     control_config_word = 0x00;
-
-	I2C_start();
-
-	/* Write the 7-bit slave address and request for write command */
-	if (I2C_send (rtc_slave_addr__write))
-	{
-		return 1;
-	}
-
-	/* Start configuring the registers */
-	if (I2C_send (control_addr))
-	{
-		return 1;
-	}
-
-	if (I2C_send (control_config_word))
-	{
-		return 1;
-	}
-
-	/* Re-start to configure remaining registers */
 	I2C_start();
 
 	if (I2C_send (rtc_slave_addr__write))
@@ -68,20 +41,82 @@ RTC_init (void)
 		return 1;
 	}
 
-	/* start the RTC and configure the initial seconds value */
-	if (I2C_send (seconds_config_word))
-	{
-		return 1;
-	}
+	if (
+		/*
+		 * Seconds register (Address: 0x00):
+		 *
+		 * Clock Hold (CH) (7): 0 (Starts the clock)
+		 * 10s digit of seconds (6-4): 0
+		 * 1s digit of seconds (3-0): 0
+		 */
+		I2C_send (0x00) ||
 
-	/* configure the initial minutes value */
-	if (I2C_send (minutes_config_word))
-	{
-		return 1;
-	}
+		/*
+		 * Minutes register (Address: 0x01):
+		 *
+		 * (7): 0
+		 * 10s digit of minutes (6-4): 5
+		 * 1s digit of minutes (3-0): 9
+		 */
+		I2C_send (0x59) ||
 
-	/* configure the type of clock (24Hours) and the initial hours value */
-	if (I2C_send (hours_config_word))
+		/*
+		 * Hours register (Address: 0x02)
+		 *
+		 * (7): 0
+		 * 12/24-hour clock (6): 0 (24-hour clock)
+		 * 10s digit of hours (5-4): 1
+		 * 1s digit of hours (3-0): 2
+		 */
+		I2C_send (0x12) ||
+
+		/*
+		 * Day register (Address: 0x03)
+		 *
+		 * (7-3): 0
+		 * Day (2-0): 03 (Wednesday; Week starts from Monday; 1-indexed)
+		 */
+		I2C_send (0x03) ||
+
+		/*
+		 * Date register (Address: 0x04)
+		 *
+		 * (7-6): 0
+		 * 10s digit of date (5-4): 2
+		 * 1s digit of date (3-0): 8
+		 */
+		I2C_send (0x28) ||
+
+		/*
+		 * Month register (Address: 0x05)
+		 *
+		 * (7-5): 0
+		 * 10s digit of month (4): 0
+		 * 1s digit of month (3-0): 3
+		 */
+		I2C_send (0x03) ||
+
+		/*
+		 * Year register (Address: 0x06)
+		 *
+		 * Note: Year is in range 00-99
+		 *
+		 * 10s digit of year (7-4): 1
+		 * 1s digit of year (3-0): 8
+		 */
+		I2C_send (0x18) ||
+
+		/*
+		 * Control register (Address: 0x07):
+		 *
+		 * Output control (OUT) (7): 0 (logic level of output pin is 0)
+		 * (6-5): 0
+		 * Square wave enable (SQWE): 0 (disable square wave output)
+		 * (3-2): 0
+		 * Rate select (1-0) (RS1, RS0): 0 (don't cares)
+		 */
+		I2C_send (0x00)
+	)
 	{
 		return 1;
 	}
